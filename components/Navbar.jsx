580ae09f510f963dea0c7449774fb689c84ccd05
lyle-timotheus <lyle.timotheus@avenutech.com>
2023-02-07 15:29:44 +0200

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { AiOutlineShopping } from 'react-icons/ai';
+
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <a href="/">
+          <Image src="/zootylala-logo.jpg" height={50} width={50} alt="" />
+        </a>
+        <Link href="/">Zootylala Designs</Link>
+      </p>
+
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
